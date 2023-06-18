320ade399e3b9f8c6a79ddb3655bf3eea2fb8e1a
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-06-18 22:10:26 +0200

    fixed a bit

--- main.py
+++ main.py
@@ -1,49 +1,51 @@
 #from colours import *
 
 def kto255(r:int, g:int, b:int):
+    k = 0
     if r not in range(0, 255):
-        raise ValueError(f’r not in range 0-255. r = {r}’)
+        raise ValueError(f'r not in range 0-255. r = {r}')
     elif g not in range(0, 255):
-        raise ValueError(f’g not in range 0-255. g = {g}’)
+        raise ValueError(f'g not in range 0-255. g = {g}')
     elif b not in range(0, 255):
-        raise ValueError(f’b not in range 0-255. b = {b}’)
+        raise ValueError(f'b not in range 0-255. b = {b}')
     
     if r == 0 and g == 0 and b == 0:
-        raise ValueError(‘Cannot process perfect black’)
+        raise ValueError('Cannot process perfect black')
     elif r >= g:
         if r >= b:
             if r != 0:
                 k = (r/255)
-    elif g >= r:
+    if g >= r:
         if g >= b:
             if g != 0:
                 k = (g/255)
-    elif b >= r:
+    if b >= r:
         if b >= g:
             if b != 0:
                 k = (b/255)
-    return(r: uniform(r/k), g: uniform(g/k), b: uniform(b/k))
+    return(round(r/k), round(g/k), round(b/k))
     
 def mto255(r:int, g:int, b:int):
+    m = 0
     if r not in range(0, 255):
-        raise ValueError(f’r not in range 0-255. r = {r}’)
+        raise ValueError(f'r not in range 0-255. r = {r}')
     elif g not in range(0, 255):
-        raise ValueError(f’g not in range 0-255. g = {g}’)
+        raise ValueError(f'g not in range 0-255. g = {g}')
     elif b not in range(0, 255):
-        raise ValueError(f’b not in range 0-255. b = {b}’)
+        raise ValueError(f'b not in range 0-255. b = {b}')
         
     if r == 0 and g == 0 and b == 0:
-        raise ValueError(‘Cannot process perfect black’)
+        raise ValueError('Cannot process perfect black')
     elif r >= g:
         if r >= b:
             if r != 0:
                 m = (255-r)
-    elif g >= r:
+    if g >= r:
         if g >= b:
             if g != 0:
                 m = (255-r)
-    elif b >= r:
+    if b >= r:
         if b >= g:
             if b != 0:
                 m = (255-b)
-    return(r: uniform(r+m), g: uniform(g+m), b: uniform(b+m))
+    return(round(r+m), round(g+m), round(b+m))
